1ee718370bec5a64d76e70d6c1f8bf15aae08d6b
kimseonj <97016371+kimseonj@users.noreply.github.com>
2022-09-05 15:56:11 +0900

    Delete resizing.py

--- resizing.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from glob import glob
-from tqdm import tqdm
-
-def resizing(img):
-    src = img
-    dst = cv2.resize(src, dsize=(2500, 2500), interpolation=cv2.INTER_LANCZOS4)
-
-    return dst

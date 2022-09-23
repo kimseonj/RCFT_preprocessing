4f94a14ecdfd0178717a827c0b037e40165ab621
kimsj <kimsunjun1221@gmail.com>
2022-09-23 15:59:03 +0900

    autopadding-PadIfNeeded에 추가되는 여백 자동화

--- libs/autopadding.py
+++ libs/autopadding.py
@@ -2,14 +2,18 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
-import glob
+from glob import glob
+from PIL import Image
+
 import albumentations as A
 from albumentations import Compose
 
-def strong_aug(p=1):
+
+
+def strong_aug(p=1, value=[255,255,255]):
     return Compose(
         [
-            A.PadIfNeeded(min_height=2500, min_width=2500, border_mode=cv2.BORDER_CONSTANT)#, value=[255,0,255], p=1)
+            A.PadIfNeeded(min_height=1650, min_width=1800, border_mode=cv2.BORDER_CONSTANT, value=value)#, p=1)
         ],
         p=p,
         additional_targets={
@@ -18,8 +22,12 @@ def strong_aug(p=1):
         }
     )
 
-def autopadding(image):
-    augmentation = strong_aug(p=1)
+def autopadding(image, black):
+    if black == True:
+        black = [255,255,255]
+    else :
+        black = [0,0,0]
+    augmentation = strong_aug(p=1, value=black)
     
     data = {'image':image, 'image_1':image}
     augmented = augmentation(**data)
@@ -28,8 +36,11 @@ def autopadding(image):
     return aug_img1
 
 if __name__ == '__main__':
-    images = sorted(glob('/data/kimsj/project/wku_rcft/image/wknz_copy_cropresult/*.jpg'))
-    
+    images = sorted(glob('/data/wknz_rcft_tiff/black_background/crop_1/immediate/*.tiff'))
+    save_path = '/data/wknz_rcft_tiff/black_background/autopadding/immediate'
+
+    os.makedirs(save_path, exist_ok=True)
+
     for path in tqdm(images):
         name = path.split('/')[-1]
 
@@ -37,5 +48,5 @@ if __name__ == '__main__':
         image = np.array(image)
         autopadding_img = autopadding(image)
 
-        cv2.imwrite('/data/kimsj/project/wku_rcft/image/padding_copy/{}'.format(name), autopadding_img)
+        cv2.imwrite(os.path.join(save_path,name), autopadding_img)
 
--- main.py
+++ main.py
@@ -17,16 +17,17 @@ from libs.convert_01 import convert_01
 from libs.resizing import resizing
 import libs.remove_background
 import libs.padding
+import libs.autopadding
 # import findContuours
 
-with open('./config/snsb_rcft.yaml') as f:
+with open('./config/wknz_rcft.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
     
 block_size = config['adaptive_threshold']['block_size']
 c = config['adaptive_threshold']['c']
 black = config['black_preprocessing']
 
-def white_preprocessing(img, rcft, name):
+def preprocessing(img, rcft, name, black):
     img = cv2.imread(img,0)
 
     img = adaptive_threshold(img, block_size, c)
@@ -34,73 +35,59 @@ def white_preprocessing(img, rcft, name):
     
     img = convert_01(result)
     img = convexhull(img)
-    if rcft == 'copy':
+    if (rcft == 'copy')|(rcft == 'cropped_copy') :
         img = libs.padding.set_copysize(img)
     crop_img = convert_01(img)
-    
-    # resizing_img = resizing(crop_img)
-    
-    # img = libs.padding.square(crop_img, black)
-    # padding_img = resizing(img)
-    
-    result_img = cv2.cvtColor(result,cv2.COLOR_GRAY2RGB)
-    crop_img = cv2.cvtColor(crop_img,cv2.COLOR_GRAY2RGB)
-    resizing_img = 1
-    padding_img = 1
-
-    return result_img, crop_img, resizing_img, padding_img
-
-def black_preprocessing(img, rcft, name):
-    img = cv2.imread(img,0)
-    
-    img = adaptive_threshold(img, block_size, c)
-    result = libs.remove_background.remove_background(img, rcft, name, config)
-    result = convert_01(result)
-
-    # img = convert_01.convert_01(result)
-    img = convexhull(result)
-    if rcft == 'copy':
-        img = libs.padding.set_copysize(img)
-    # crop_img = convert_01.convert_01(img)
-    crop_img = img
+    # crop_img = img
 
     resizing_img = resizing(crop_img)
-
+    
     img = libs.padding.square(crop_img, black)
     padding_img = resizing(img)
 
-    result_img = cv2.cvtColor(result,cv2.COLOR_GRAY2RGB)
-    crop_img = cv2.cvtColor(crop_img,cv2.COLOR_GRAY2RGB)
-    resizing_img = cv2.cvtColor(resizing_img,cv2.COLOR_GRAY2RGB)
-    padding_img = cv2.cvtColor(padding_img,cv2.COLOR_GRAY2RGB)
+    autopadding_img = libs.autopadding.autopadding(crop_img, black)
 
-    return result_img, crop_img, resizing_img, padding_img
+    # crop_img = crop_img[753:,]
+    # crop_img = convexhull(crop_img)
+    # crop_img = convert_01(crop_img)
+
+    result_img = cv2.cvtColor(result, cv2.COLOR_GRAY2RGB)
+    crop_img = cv2.cvtColor(crop_img, cv2.COLOR_GRAY2RGB)
+    resizing_img = cv2.cvtColor(resizing_img, cv2.COLOR_GRAY2RGB)
+    padding_img = cv2.cvtColor(padding_img, cv2.COLOR_GRAY2RGB)
+    autopadding_img = cv2.cvtColor(autopadding_img, cv2.COLOR_GRAY2RGB)
+
+    if black == False:
+        return result_img, crop_img, resizing_img, padding_img, autopadding_img
+    elif black == True:
+        return convert_01(result_img), convert_01(crop_img),\
+               convert_01(resizing_img), convert_01(padding_img), convert_01(autopadding_img)
+
 
 def save_img(img_path, out_path, rcft):
     
     images = sorted(glob(os.path.join(img_path,rcft,'*.tiff')))
     
     os.makedirs(os.path.join(out_path,'result',rcft), exist_ok=True)
-    # os.makedirs(os.path.join(out_path,'crop',rcft), exist_ok=True)
+    os.makedirs(os.path.join(out_path,'crop',rcft), exist_ok=True)
     # os.makedirs(os.path.join(out_path,'padding',rcft), exist_ok=True)
+    os.makedirs(os.path.join(out_path,'autopadding',rcft), exist_ok=True)
 
     # try:
     for img in tqdm(images):
+        # jpg / tiff 둘다 사용하기 위해 확장자 나눠 줌
         name_extention = img.split('/')[-1]
 
         name = name_extention.split('.')[0]
         extention = name_extention.split('.')[1]
         
-        if black == True:
-            result_img, crop_img, resizing_img, padding_img = black_preprocessing(img, rcft, name)
-        else:
-            result_img, crop_img, resizing_img, padding_img = white_preprocessing(img, rcft, name)
+        result_img, crop_img, resizing_img, padding_img, autopadding_img = preprocessing(img, rcft, name, black)
 
-        cv2.imwrite(os.path.join(out_path,'result',rcft,name+'.'+extention),result_img)
+        cv2.imwrite(os.path.join(out_path,'result',rcft,name+'.'+extention), result_img)
+        cv2.imwrite(os.path.join(out_path,'crop',rcft,name+'.'+extention), crop_img)
+        # cv2.imwrite(os.path.join(out_path,'padding',rcft,name+'.'+extention), padding_img)
+        cv2.imwrite(os.path.join(out_path,'autopadding',rcft,name+'.'+extention), autopadding_img)
 
-        # cv2.imwrite(out_path+'/crop/'+rcft+'/'+name+'.jpg',crop_img)
-        # cv2.imwrite(out_path+'/resizing/'+rcft+'/'+name+'.jpg',resizing_img)
-        # cv2.imwrite(out_path+'/padding/'+rcft+'/'+name+'.jpg',padding_img)
     # except:
     #     error.append(name)    
 
